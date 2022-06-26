e8ea2259008d2cb0b3b83aa3848cbca139b24517
Janaki04 <telugujanaki412@gmail.com>
2022-06-26 17:05:53 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
 import './App.css';
 import Form from './Component/Form';
-import Phone from './Component/Phone';
+
 
 function App() {
   return (
     <div className="App">
      <Form/>
 
-     {/* <Phone/> */}
+     
 
     </div>
   );
--- src/Component/Form.js
+++ src/Component/Form.js
@@ -42,27 +42,27 @@ function Form (){
     return(
         <div>
         <h1 className="heading">PHONE BOOK</h1><br/>             
-    <form class="form-group" onSubmit={dataSubmit}>
+    <form className="form-group" onSubmit={dataSubmit}>
 
-        <label class="form-group text-dark font-weight-bold" >FirstName:</label>
-        <input required className='first' type="firstname"  name="firstname" value={data.firstname} onChange={dataChange} />  &nbsp;  &nbsp; 
+        <label className="form-group text-dark font-weight-bold" >FirstName:</label>
+        <input required  type="firstname"  name="firstname" value={data.firstname} onChange={dataChange} />  &nbsp;  &nbsp; 
 
-        <label class="form-group text-dark font-weight-bold">LastName:</label>
-        <input required className='second' type="lastname" name="lastname" value={data.lastname} onChange={dataChange}/>   &nbsp;  &nbsp;                                                                                                   
+        <label className="form-group text-dark font-weight-bold">LastName:</label>
+        <input required  type="lastname" name="lastname" value={data.lastname} onChange={dataChange}/>   &nbsp;  &nbsp;                                                                                                   
         
-        <label class="form-group text-dark font-weight-bold">Phone Number:</label>
+        <label className="form-group text-dark font-weight-bold">Phone Number:</label>
         <input className='' type="number" name="number" value={data.number} onChange={dataChange}/><br/>
 
-        <input class="btn btn-primary" type="submit" value="Add Contact"/>
+        <input className="btn btn-primary" type="submit" value="Add Contact"/>
        
     </form>
     <div >
     
      {output.map(item=>(                                                                                                                                                                                                                                                          
-    <h5 key={item.id} class="text-center font-italic ">                                                                                                                                                                                               
+    <h5 key={item.id} className="text-center font-italic ">                                                                                                                                                                                               
                                                                                                                       
     
-    <table class="table table-bordered">
+    <table className="table table-bordered">
   <thead>
     <tr>                                                                                                                                                                                                                                                         
       <th scope="col">No</th>
@@ -81,7 +81,7 @@ function Form (){
       <td>{item.firstname}</td>
       <td>{item.lastname}</td>
       <td>{item.number}</td>
-      <td><button class="btn btn-danger" onClick={()=>dataDelete(item.id)}>Delete</button></td>
+      <td><button className="btn btn-danger" onClick={()=>dataDelete(item.id)}>Delete</button></td>
 
     </tr>
   </tbody>
